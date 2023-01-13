b1802a13bf18b81e50c7b17bf61de169b9a929c2
Ewelina Nowak <ewelinanowak@Ewelinas-MacBook-Pro.local>
2023-01-13 19:05:27 +0100

    completed navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,6 +3,9 @@
 import Link from 'next/link';
 
 export default function Navbar() {
+  const burger =
+    'min-w-[700px] pr-[100px] mr-[0px] -mt-[70px] min-[470px]:pl-[300px] pl-[400px] pt-[80px] pb-[400px]';
+
   return (
     <div className="flex justify-between py-8">
       <Link href="/">
@@ -12,7 +15,23 @@ export default function Navbar() {
           className="md:w-[237px] md:h-[25px] w-[168px] h-[20px]"
         />
       </Link>
-      <nav role="navigation" className="block min-[380px]:hidden">
+
+      <nav className="hidden sm:flex items-end uppercase text-xs text-darkGray gap-8">
+        <Link href="/about">
+          <p>About us</p>
+        </Link>
+        <Link href="/about">
+          <p>Shop</p>
+        </Link>
+        <Link href="/about">
+          <p>Blog</p>
+        </Link>
+        <Link href="#">
+          <p>Create your plan</p>
+        </Link>
+      </nav>
+
+      <nav role="navigation" className="block sm:hidden">
         <div id="menuToggle">
           <input type="checkbox" />
 
@@ -20,7 +39,7 @@ export default function Navbar() {
           <span></span>
           <span></span>
 
-          <ul id="menu">
+          <ul id="menu" className={burger}>
             <a href="#">
               <li>Home</li>
             </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{html,js,jsx}",
-    "./components/**/*.{html,js,jsx}",
-    "./sections/**/*.{html,js,jsx}",
-    "./styles/**/*.{js,jsx}",
+    './app/**/*.{html,js,jsx}',
+    './components/**/*.{html,js,jsx}',
+    './sections/**/*.{html,js,jsx}',
+    './styles/**/*.{js,jsx}',
   ],
-  mode: "jit",
+  mode: 'jit',
   theme: {
     colors: {
-      teal: "#0E8784",
-      tealHover: "#66D2CF",
-      gray: "rgb(199, 191, 190)",
-      darkBlue: "#333D4B",
-      white: "#FEFCF7",
-      cream: "#fdd5ba",
+      teal: '#0E8784',
+      tealHover: '#66D2CF',
+      gray: 'rgb(199, 191, 190)',
+      darkGray: '#b3b7ba',
+      darkBlue: '#333D4B',
+      white: '#FEFCF7',
+      cream: '#fdd5ba',
     },
     extend: {
       transitionTimingFunction: {
-        "out-flex": "cubic-bezier(0.05, 0.6, 0.4, 0.9)",
+        'out-flex': 'cubic-bezier(0.05, 0.6, 0.4, 0.9)',
       },
       backgroundImage: {
-        "hero-lg": "url('/image-hero-lg.jpg')",
-        "hero-md": "url('/image-hero-md.jpg')",
-        "hero-sm": "url('/image-hero-sm.jpg')",
+        'hero-lg': "url('/image-hero-lg.jpg')",
+        'hero-md': "url('/image-hero-md.jpg')",
+        'hero-sm': "url('/image-hero-sm.jpg')",
       },
     },
   },
