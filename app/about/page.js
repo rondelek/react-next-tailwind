7b284be914c197446d7e764cb6c7fbab6e99239d
Ewelina Nowak <ewelinanowak@Ewelinas-MacBook-Pro.local>
2023-01-13 19:21:12 +0100

    started about page

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,17 @@
-import React from "react";
+'use client';
 
-export default function about() {
-  return <div>test</div>;
+export default function About() {
+  return (
+    <section className="rounded-xl p-10 lg:bg-about-lg bg-right md:bg-about-md bg-about-sm bg-cover">
+      <div className="text-center md:text-left md:max-w-[50%] my-10">
+        <h1 className="text-4xl font-bold text-white">About us </h1>
+        <p className="text-md md:text-lg text-gray py-6">
+          Coffeeroasters began its journey of exotic discovery in 1999,
+          highlighting stories of coffee from around the world. We have since
+          been dedicated to bring the perfect cup - from bean to brew - in every
+          shipment.
+        </p>
+      </div>
+    </section>
+  );
 }
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,7 +1,4 @@
 'use client';
-import { motion } from 'framer-motion';
-import styles from '../styles';
-import { slideIn, staggerContainer, textVariant } from '../utils/motion';
 
 export default function Hero() {
   return (
